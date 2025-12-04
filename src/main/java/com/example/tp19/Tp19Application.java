9bd2970e46984d7c7c0f14638caaa061d0a3d095
Ghita <takouitghita@gmail.com>
2025-12-04 14:54:37 +0100

    tp19 server

---
+++ src/main/java/com/example/tp19/Tp19Application.java
@@ -0,0 +1,16 @@
+package com.example.tp19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Tp19Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Tp19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp19/Tp19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
